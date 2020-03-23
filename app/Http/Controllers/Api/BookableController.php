f4ee824d8bf02ac512c4720bfb251580315df4dd
sangTran <sangthien2010@gmail.com>
2020-03-22 19:47:29 -0500

    adding new apiResources in Controllers

--- app/Http/Controllers/Api/BookableController.php
+++ app/Http/Controllers/Api/BookableController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Bookable;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookableIndexResource;
+use App\Http\Resources\BookableShowResource;
 use Illuminate\Http\Request;
 
 class BookableController extends Controller
 {
     public function index()
     {
-        return Bookable::all();
+        return BookableIndexResource::collection(
+            Bookable::all()
+        );
     }
 
     public function show($id)
     {
-        return Bookable::findOrFail($id);
+        return new BookableShowResource(Bookable::findOrFail($id));
     }
 }
