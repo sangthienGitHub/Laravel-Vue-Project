85fc27dcf3f4ceb772e722d606bdc6f52088c250
sangTran <sangthien2010@gmail.com>
2020-03-24 21:11:01 -0500

    adding the new relationship of review model to bookable and booking

--- app/Booking.php
+++ app/Booking.php
@@ -13,6 +13,12 @@ class Booking extends Model
     {
         return $this->belongsTo(Bookable::class);
     }
+
+    public function review()
+    {
+        return $this->hasOne(Review::class);
+    }
+
     public function scopeBetweenDates(Builder $query, $from, $to)
     {
         return $query->where('to', '>=', $from)->where('from', '<=', $to);
